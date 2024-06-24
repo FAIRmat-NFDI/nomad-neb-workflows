8d12ba19cb2046c45528357e677c2d0d7b97be42
JosePizarro3 <jose.pizarro@physik.hu-berlin.de>
2024-06-24 12:57:19 +0200

    Added extract_total_energy_differences and quantity in the schema

--- src/nomad_neb_workflows/schema_packages/neb.py
+++ src/nomad_neb_workflows/schema_packages/neb.py
@@ -1,4 +1,5 @@
 from typing import TYPE_CHECKING
+import numpy as np
 
 if TYPE_CHECKING:
     from nomad.datamodel.datamodel import (
@@ -26,13 +27,43 @@ class NEBWorkflow(SimulationWorkflow):
     )
     message = Quantity(type=str)
 
+    total_energy_differences = Quantity(
+        type=np.float64,
+        shape=['*'],
+        unit='joule',
+        description="""
+        Total energy differences of the system configuration for each of the steps in the path
+        """,
+    )
+
     def __init__(self, m_def: Section = None, m_context: Context = None, **kwargs):
         super().__init__(m_def, m_context, **kwargs)
         # Set the name of the section
         self.name = self.m_def.name
 
+    def extract_total_energy_differences(self):
+        tot_energies = []
+        for output in self.outputs:
+            tot_energies.append(output.section.energy.total.value)
+        return tot_energies
+
     def normalize(self, archive: 'EntryArchive', logger: 'BoundLogger') -> None:
         super().normalize(archive, logger)
+
+        print('hello!')
+
+        try:
+            self.total_energy_differences = self.extract_total_energy_differences()
+        except Exception:
+            logger.error('Could not set `NEBWorkflow.total_energy_differences`.')
+
+        try:
+            archive.metadata.entry_type = 'NEB'
+            archive.metadata.entry_name = 'name test'
+        except Exception:
+            logger.error(
+                'Could not set archive.metadata quantities entry_type and entry_name.'
+            )
 
         logger.info('MySchema.normalize', parameter=configuration.parameter)
         self.message = f'Hello {self.name}!'
